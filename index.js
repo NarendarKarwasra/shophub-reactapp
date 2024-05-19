24051353cc2a207ff194c83e37dac019082a5631
NarendarKarwasra <153517618+NarendarKarwasra@users.noreply.github.com>
2024-05-19 16:48:24 +0530

    Made changes

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use('/images',express.static('upload/images'))
 app.post("/upload", upload.single('product'),(req, res)=>{
     res.json({
         success : 1,
-        image_url : `https://localhost:${port}/images/${req.file.filename}`
+        image_url : `https://shophub-reactapp.onrender.com/images/${req.file.filename}`
     })
 })
 
